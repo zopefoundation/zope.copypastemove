17c3274a14eb81c66d723e8126483992ab6b71ec
Hanno Schlichting <hanno@hannosch.eu>
2009-12-16 23:26:49 +0000

    Prepare zope.copypastemove 3.6.0.

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@
 # Zope Toolkit policies as described by this documentation.
 ##############################################################################
 
-version = '3.6.0dev'
+version = '3.6.0'
 
 from setuptools import setup, find_packages
 
