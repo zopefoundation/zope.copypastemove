3c311fe8ecd076ad79631418b30a6e6959d54143
Jason Madden <jamadden@gmail.com>
2017-08-04 05:51:37 -0500

    Preparing release 4.1.0

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ TESTS_REQUIRE = ZCML_REQUIRES + [
 ]
 
 setup(name='zope.copypastemove',
-      version='4.1.0.dev0',
+      version='4.1.0',
       url='http://github.com/zopefoundation/zope.copypastemove',
       license='ZPL 2.1',
       author='Zope Foundation and Contributors',
