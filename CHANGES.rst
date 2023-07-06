4928d043ffd363de8e321dea68f401d7284d5489
Michael Howitz <icemac@gmx.net>
2023-07-06 14:09:55 +0200

    Preparing release 5.0

--- CHANGES.rst
+++ CHANGES.rst
@@ -2,7 +2,7 @@
  Changes
 =========
 
-5.0 (unreleased)
+5.0 (2023-07-06)
 ================
 
 - Add support for Python 3.11.
--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ TESTS_REQUIRE = ZCML_REQUIRES + [
 ]
 
 setup(name='zope.copypastemove',
-      version='5.0.dev0',
+      version='5.0',
       url='http://github.com/zopefoundation/zope.copypastemove',
       license='ZPL 2.1',
       author='Zope Foundation and Contributors',
